eac7c5800207b387a46dfcb7f3f4bb2541e6c2b7
ChengxiYu <ykwxh99@outlook.com>
2023-07-16 11:34:21 +0200

    Test tests

---
+++ app/app/tests.py
@@ -0,0 +1,11 @@
+
+from django.test import SimpleTestCase
+
+from app import multiplication
+
+class MultiTests(SimpleTestCase):
+
+    def test_multi_numbers(self):
+        ans = multiplication.multi(10, 10)
+
+        self.assertEqual(ans, 100)
