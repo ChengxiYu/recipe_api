96078b7d58a90e22f63e9b648fef32e40c98069b
ChengxiYu <ykwxh99@outlook.com>
2023-07-20 12:21:02 +0200

    Lingting

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -6,6 +6,7 @@ from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
 
+
 @patch('core.management.commands.wait_for_db.Command.check')
 class CommandTest(SimpleTestCase):
     """Test commands"""
@@ -17,15 +18,14 @@ class CommandTest(SimpleTestCase):
         call_command('wait_for_db')
 
         mock_check.assert_called_once_with(databases=['default'])
-    
+
     @patch('time.sleep')
     def test_wait_for_db_delay(self, mock_sleep, mock_check):
         """Test command when delay"""
         mock_check.side_effect = [Psycopg2Error] * 2 + \
             [OperationalError] * 3 + [True]
-        
+
         call_command('wait_for_db')
 
         self.assertEqual(mock_check.call_count, 6)
         mock_check.assert_called_with(databases=['default'])
-        
